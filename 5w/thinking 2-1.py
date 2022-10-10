fa044adc404ec90e4c64e1af89921442f936fb6f
moolgutree <85077262+moolgutree@users.noreply.github.com>
2022-10-10 19:50:59 +0900

    Create thinking 2-1.py

---
+++ 5w/thinking 2-1.py
@@ -0,0 +1,30 @@
+import csv
+
+f = open('subwaytime.csv')
+data = csv.reader(f)
+header = next(data)
+header = next(data)
+
+mx=0
+low=99999999999999999999999999999999999
+mxs=''
+lows=''
+repo=[]
+lowRepo=[]
+
+for row in data:
+    for i in range(10, 12):
+        row[i] = int(row[i].replace(',',''))
+    if row[10] > mx:
+        mx = row[10]
+        repo = row[10]
+        mxs = row[1]+ ' ' + row[3]
+    if row[11] < low and row[11] > 1:
+        low = row[11]
+        lowRepo = row[11]
+        lows = row[1] + ' ' + row[3]
+        
+print(mxs, round(repo,2))
+print(lows, round(lowRepo,2))
+
+f.close()
